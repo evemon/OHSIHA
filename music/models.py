74392d205d73913862dc37af32740d82e078e7da
Eveliina Saari <eveliina.saari@student.tut.fi>
2018-03-04 23:53:35 +0200

    adding two albums by using powershell

--- music/models.py
+++ music/models.py
@@ -5,7 +5,9 @@ class Album(models.Model):
     artist = models.CharField(max_length=200)
     title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title + ' - ' + self.artist
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
