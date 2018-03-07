336d81b11f7528b83ad3e22b32c34a31a4b0d599
Eveliina Saari <eveliina.saari@student.tut.fi>
2018-03-08 00:02:01 +0200

    created html page for home page

--- music/views.py
+++ music/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
+#collects all albums to the Music-page
 def index(request):
-    return HttpResponse("<h1>Music Page</h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    templateInfo = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(templateInfo, request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Album details for album id: " + str(album_id) + "</h2>")
